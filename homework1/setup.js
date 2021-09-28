210870b16ffac184c09b2a5a4f6b0c8c173b1294
MaxYashch <43985806+MaxYashch@users.noreply.github.com>
2021-09-28 23:00:23 +0300

    Changed quotes
    Changed quotes

--- homework1/setup.js
+++ homework1/setup.js
@@ -1,29 +1,29 @@
 function first() {
-    let firstValue = +prompt("Введите значение");
-    let secondValue = +prompt("Введите значение");
+    let firstValue = +prompt('Введите значение')
+    let secondValue = +prompt('Введите значение')
     if (!isNaN(firstValue) && !isNaN(secondValue)) {
-        console.log(firstValue.toString(secondValue));
+        console.log(firstValue.toString(secondValue))
     } else {
-        console.log("Некорректный ввод!");
+        console.log('Некорректный ввод!')
     }
-    return;
+    return
 }
 
 function second() {
-    let firstValue = +prompt("Введите значение");
+    let firstValue = +prompt('Введите значение')
     if (isNaN(firstValue)) {
-        console.log("Некорректный ввод!");
-        return;
+        console.log('Некорректный ввод!')
+        return
     }
-    let secondValue = +prompt("Введите значение");
+    let secondValue = +prompt('Введите значение')
     if (isNaN(secondValue)) {
-        console.log("Некорректный ввод!");
-        return;
+        console.log('Некорректный ввод!')
+        return
     } else if (!isNaN(firstValue) && !isNaN(secondValue)) {
         console.log(
             `Ответ: ${firstValue + secondValue}, ${firstValue / secondValue}`
-        );
-        return;
+        )
+        return
     }
-    return;
+    return
 }
