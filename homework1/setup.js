5cd43aef1fe5048f39ab615473c7007f43b0c458
MaxYashch <maxyashch@gmail.com>
2021-10-04 00:35:52 +0300

    delete useless return

--- homework1/setup.js
+++ homework1/setup.js
@@ -1,29 +1,27 @@
 function first() {
-    let firstValue = +prompt("Введите значение");
-    let secondValue = +prompt("Введите значение");
+    let firstValue = +prompt('Введите значение')
+    let secondValue = +prompt('Введите значение')
     if (!isNaN(firstValue) && !isNaN(secondValue)) {
-        console.log(firstValue.toString(secondValue));
+        console.log(firstValue.toString(secondValue))
     } else {
-        console.log("Некорректный ввод!");
+        console.log('Некорректный ввод!')
     }
-    return;
 }
 
 function second() {
-    let firstValue = +prompt("Введите значение");
+    let firstValue = +prompt('Введите значение')
     if (isNaN(firstValue)) {
-        console.log("Некорректный ввод!");
-        return;
+        console.log('Некорректный ввод!')
+        return
     }
-    let secondValue = +prompt("Введите значение");
+    let secondValue = +prompt('Введите значение')
     if (isNaN(secondValue)) {
-        console.log("Некорректный ввод!");
-        return;
-    } else if (!isNaN(firstValue) && !isNaN(secondValue)) {
+        console.log('Некорректный ввод!')
+        return
+    }
+    if (!isNaN(firstValue) && !isNaN(secondValue)) {
         console.log(
             `Ответ: ${firstValue + secondValue}, ${firstValue / secondValue}`
-        );
-        return;
+        )
     }
-    return;
 }
