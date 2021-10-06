79fe2a9b656063c0f93659ef96cd5ad9a468a13f
MaxYashch <maxyashch@gmail.com>
2021-10-06 15:23:13 +0300

    added homeork 3

---
+++ homework3/setup.js
@@ -0,0 +1,23 @@
+Array.prototype.myFilter = function (callback, thisObject) {
+  let filteredArray = [];
+  if (thisObject) {
+    callback = callback.bind(thisObject);
+  }
+  let thisLength = this.length;
+  for (let i = 0; i < thisLength; i++) {
+    if (i in this) {
+      if (callback(this[i], i, this)) {
+        filteredArray.push(this[i]);
+      }
+    }
+  }
+  return filteredArray;
+};
+
+const createDebounceFunction = (callback, msc) => {
+  let timeout;
+  return () => {
+    clearTimeout(timeout);
+    timeout = setTimeout(callback, msc);
+  };
+};
